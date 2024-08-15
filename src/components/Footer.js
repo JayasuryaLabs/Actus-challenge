8efc454599060d24e3c3f689093a268a80d161a4
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2024-08-15 20:44:35 +0530

    version -1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; ACTUS Hackathon. All Rights Reserved.</p>
+      <p>Made with ❤️ by jais and rash</p>
       {/* Add more footer elements as required */}
     </footer>
   );
